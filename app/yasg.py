ed30e2c1330c25eeead49864955612953f8e1104
MooVi <mooovidev@gmail.com>
2021-08-10 09:04:20 +0300

    Fix swagger's urls

--- app/yasg.py
+++ app/yasg.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-from django.urls import path
+from django.urls import path, re_path
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
@@ -22,18 +22,18 @@ from drf_yasg import openapi
 
 SchemaView = get_schema_view(
    openapi.Info(
-      title="Django Movie",
+      title="Photo album",
       default_version='v1',
       description="Test description",
-      license=openapi.License(name="BSD License"),
+      license=openapi.License(name="MooVi-dev"),
    ),
    public=True,
    permission_classes=(permissions.AllowAny,),
 )
 
 urlpatterns = [
-   path(r'swagger(?P<format>\.json|\.yaml)',
-        SchemaView.without_ui(cache_timeout=0), name='schema-json'),
+   re_path(r'swagger(?P<format>\.json|\.yaml)/$',
+           SchemaView.without_ui(cache_timeout=0), name='schema-json'),
    path(r'swagger/', SchemaView.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
    path(r'redoc/', SchemaView.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
